4f43fd7ec02f2b3788af29b507c5722b65762f57
vjabuilds <jovan.vjestica@outlook.com>
2022-10-19 15:00:46 +0200

    Initialized the project

---
+++ src/main/java/dev/vjabuilds/datawavesauth/DatawavesAuthApplication.java
@@ -0,0 +1,13 @@
+package dev.vjabuilds.datawavesauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawavesAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawavesAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vjabuilds/datawavesauth/DatawavesAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vjabuilds.datawavesauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawavesAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
