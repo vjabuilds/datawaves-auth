7fc747578ef53b79649153791976a51a56de1d4f
vjabuilds <jovan.vjestica@outlook.com>
2022-11-08 13:13:08 +0100

    Added eager fetching for roles

--- src/main/java/dev/vjabuilds/datawavesauth/models/DatawavesUser.java
+++ src/main/java/dev/vjabuilds/datawavesauth/models/DatawavesUser.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -32,7 +33,7 @@ public class DatawavesUser {
     private String password;
     private boolean enabled = true;
 
-    @ManyToMany( cascade = {CascadeType.ALL})
+    @ManyToMany( cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
     @JoinTable(
         name = "UserRole",
         joinColumns = {@JoinColumn(name = "user_id")},
